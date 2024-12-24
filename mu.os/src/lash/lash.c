95a99ec05fd87b106b840256bca9943faeb716d5
alisaddam590 <119085009+alisaddam590@users.noreply.github.com>
2024-12-24 14:44:11 +0000

    Update lash.c

--- mu.os/src/lash/lash.c
+++ mu.os/src/lash/lash.c
@@ -31,6 +31,23 @@ int main() {
         } else if (str_eq(tokens[0], "echo")) {
             // Handle echo command: print the rest of the input
             echo_command(buff);  // Call echo function to display the argument
+        }else if (str_eq(tokens[0], "cd")) {
+             if (token_count > 1 && token_count <3) {
+            char *new_path = tokens[1];
+
+            if (sys_chdir(new_path) == 0) {
+                str_print("Directory changed to: ");
+                str_print(new_path);
+                str_print("\n");
+                str_copy(cwd, new_path);  // Optionally update cwd buffer
+            } else {
+                str_print("cd: No such file or directory: ");
+                str_print(new_path);
+                str_print("\n");
+                }   }
+            else  {
+            str_print("cd: Missing argument\n");
+            }       
         }
     }
     return 0;
