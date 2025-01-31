68c8e52a23699a45d2a0ce1e77b5faec612c29d8
alisaddam590 <119085009+alisaddam590@users.noreply.github.com>
2025-01-31 23:06:12 +0000

    Update init.c

--- mu.os/src/init/init.c
+++ mu.os/src/init/init.c
@@ -2,6 +2,32 @@
 #include <fcntl.h>
 #include <mylib.h>
 
+// Function to split a line into tokens based on spaces
+int tokenize_input(char *line, char input[][1024], int max_tokens) {
+    int token_count = 0;  // Number of tokens found
+    int i = 0, j = 0;     // Indices for input array and current token
+
+    while (*line != '\0' && token_count < max_tokens) {
+        // Skip leading spaces (spaces, tabs, or newlines)
+        while (*line == ' ' || *line == '\t' || *line == '\n') {
+            line++;
+        }
+
+        // Read the next word
+        while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n') {
+            input[token_count][j++] = *line++;
+        }
+
+        // Null-terminate the token if we have read a word
+        if (j > 0) {
+            input[token_count][j] = '\0';
+            token_count++;
+            j = 0;  // Reset for the next token
+        }
+    }
+
+    return token_count;  // Return the number of tokens found
+}
 
 
 int main() {
@@ -20,9 +46,11 @@ int main() {
 char line[] = "  echo   this is a   test  ";
     char tokens[10][1024];
     int token_count = tokenize_input(line, tokens, 10);
-    
+
+
+
     for (int i = 0; i < token_count; i++) {
-        str_print("\n");
+
         str_print(tokens[i]);
     }
 
@@ -39,3 +67,6 @@ char line[] = "  echo   this is a   test  ";
 
     return 0;
 }
+
+
+
