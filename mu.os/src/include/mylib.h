a9d76502a49a733ea63e3d37fec00943473fec44
alisaddam590 <119085009+alisaddam590@users.noreply.github.com>
2025-01-31 23:04:55 +0000

    Update mylib.h

--- mu.os/src/include/mylib.h
+++ mu.os/src/include/mylib.h
@@ -1,22 +1,13 @@
 #pragma once
 #include <time.h>
-#include <sys/types.h>
-
-
-extern char history[10][1024];  // Store up to 10 commands
-extern int history_index;            // The index where the next command will be stored
-extern unsigned long console_fd;
-extern char cwd[1024]; 
-
+#include<stdio.h>
 
 extern unsigned long _syscall(int num, void *a0, void *a1, void *a2, void *a3, void *a4, void *a5);
 
 extern unsigned long sys_open(char *fn, int flags);
 extern unsigned long sys_read(unsigned long fd, char *buff, unsigned long size);
 extern unsigned long sys_reboot();
-extern  long sys_nanosleep(struct timespec *req, struct timespec *rem);
-extern unsigned long sys_mkdir(char *path, mode_t mode);
-extern unsigned long sys_getcwd(char *buf, unsigned long size);
+unsigned long sys_nanosleep(struct timespec *req, struct timespec *rem);
 
 extern unsigned long str_len(char *sz);
 extern void str_print(char *str);
@@ -32,22 +23,22 @@ extern int execute_process(char *filename);
 
 
 
-extern char* str_copy(char* dest, const char* src);
+char* str_copy(char* dest, const char* src);
 
 
-extern void console_open() ;
+void console_open() ;
 
-extern char console_read() ;
-extern void console_write(char *str) ;
-extern void console_write_char(char c) ;
-extern void add_to_history(char *command) ;
-extern int read_line(char *buff, int max) ;
-extern void clear_shell();
-extern void echo_command(char *buff) ;
-extern int tokenize_input(char *line, char input[][1024], int max_tokens) ;
-extern void pwd_command () ;
-extern long sys_chdir(char *path);
+char console_read() ;
+void console_write(char *str) ;
+void console_write_char(char c) ;
+void add_to_history(char *command) ;
+int read_line(char *buff, int max) ;
+void clear_shell();
+void echo_command(char *buff) ;
+int tokenize_input(char *line, char input[][1024], int max_tokens) ;
 
-extern void cd_command(char *token, int token_count);
+extern void dir(char path[1024]);
+extern void Time();
 
-void mkdir_command(char *dir_name, int token_count);
+
+
