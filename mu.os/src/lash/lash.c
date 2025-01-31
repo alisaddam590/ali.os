82b973ca9d9256ccff1e066aec9b8fba02b91a8e
alisaddam590 <119085009+alisaddam590@users.noreply.github.com>
2025-01-31 23:07:54 +0000

    Update lash.c

--- mu.os/src/lash/lash.c
+++ mu.os/src/lash/lash.c
@@ -1,14 +1,17 @@
 #include <mylib.h>
+#include <unistd.h>
+#include<stdio.h>
+#include<time.h>
+
 
 int main() {
-    str_print("\033[H\033[J"); // Clear screen
-    str_print("LASH v0.2.0\n");
+    printf("\033[H\033[J"); // Clear screen
+    printf("LASH v0.2.0\n");
+   
 
-    str_copy(cwd,"/user");
     console_open();
 
     while (1) {
-        str_print(cwd);
         str_print(" :> ");
         char buff[1024];
         char tokens[10][1024];
@@ -19,36 +22,23 @@ int main() {
             continue;
         }
 
-        // Check for exit command
+        // Check for echo command
         if (str_eq(tokens[0], "exit")) {
             str_print("Exiting shell...\n");
             break;
-        } 
-        // Check for reboot command
-        else if (str_eq(tokens[0], "reboot")) {
+        } else if (str_eq(tokens[0], "reboot")) {
             str_print("\n\n*** SYSTEM REBOOTING ***\n");
             sys_reboot();
-        } 
-        // Check for clear command
-        else if (str_eq(tokens[0], "clear")) {
+        } else if (str_eq(tokens[0], "clear")) {
             clear_shell();
-        } 
-        // Check for echo command
-        else if (str_eq(tokens[0], "echo")) {
-            echo_command(buff);  // Call echo function to display the argument
-        } 
-        // Check for cd command
-         else if (str_eq(tokens[0], "cd")) {
-            cd_command(tokens[1], token_count);  // Call cd_command function
-        }
-
-        else if (str_eq(tokens[0], "pwd")) {
-            str_print(cwd);
-        }
-        else if (str_eq(tokens[0], "mkdir")) {
-            mkdir_command(tokens[1], token_count);  // Call mkdir_command function
+        } else if (str_eq(tokens[0], "echo")) {
+            // Handle echo command: print the rest of the input
+            echo_command(buff);     // Call echo function to display the argument
+        }else if(str_eq(tokens[0],"time")){
+            Time();
         }
     }
-
     return 0;
 }
+
+
