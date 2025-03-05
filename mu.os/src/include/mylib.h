ece9d565ac47e5db0982e02701f3d192884b9b59
alisaddam590 <119085009+alisaddam590@users.noreply.github.com>
2025-03-05 21:24:42 +0000

    Update mylib.h

--- mu.os/src/include/mylib.h
+++ mu.os/src/include/mylib.h
@@ -1,13 +1,23 @@
 #pragma once
 #include <time.h>
-#include<stdio.h>
+#include <stdio.h>
+
+// External variables
+extern char ROOT_PATH[];
+extern char START_PATH[];
+extern char current_directory[];  // Ensure it's an array, not a pointer to a pointer
+
+// Maximum path length
+#define MAX_PATH_LENGTH 256
+
+// Function declarations
+extern void init_paths();
 
 extern unsigned long _syscall(int num, void *a0, void *a1, void *a2, void *a3, void *a4, void *a5);
-
 extern unsigned long sys_open(char *fn, int flags);
 extern unsigned long sys_read(unsigned long fd, char *buff, unsigned long size);
 extern unsigned long sys_reboot();
-unsigned long sys_nanosleep(struct timespec *req, struct timespec *rem);
+extern unsigned long sys_nanosleep(struct timespec *req, struct timespec *rem);
 
 extern unsigned long str_len(char *sz);
 extern void str_print(const char *format, ...);
@@ -20,29 +30,32 @@ extern unsigned long sys_write(unsigned long fd, char *buf, unsigned long len);
 extern long sys_fork();
 extern long sys_execve(char *filename, char **argv, char **envp);
 extern int execute_process(char *filename);
+extern int execute_process_with_args(const char *path, const char *arg, const char *arg2);
 
+extern char* str_copy(char* dest, const char* src);
 
-
-char* str_copy(char* dest, const char* src);
-
-
-void console_open() ;
-
-char console_read() ;
-void console_write(char *str) ;
-void console_write_char(char c) ;
-void add_to_history(char *command) ;
-int read_line(char *buff, int max) ;
-void clear_shell();
-void echo_command(char *buff) ;
-int tokenize_input(char *line, char input[][1024], int max_tokens) ;
+extern void console_open();
+extern char console_read();
+extern void console_write(char *str);
+extern void console_write_char(char c);
+extern void add_to_history(char *command);
+extern int read_line(char *buff, int max);
+extern void clear_shell();
+extern void echo_command(char *buff);
+extern int tokenize_input(char *line, char input[][1024], int max_tokens);
 
 extern void dir(char path[1024]);
 extern void Time();
+
+// Directory functions
 extern int change_directory(const char *path);
-extern char* print_current_directory();
+extern void init_paths();
+extern void print_current_directory();
 extern void pwd();
 extern int ls(const char *path);
-extern int dir_exists(const char *path);
+extern int dir_exists(const char *path); // Removed duplicate definition
 extern int create_directory(const char *path);
 extern int mkdir_create_folder(const char *folder_name);
+extern void print_disk_usage(const char *path);
+extern void lsblk();
+extern void print_block_device_info(const char *device_name);
